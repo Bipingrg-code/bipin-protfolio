d459f433b3743327b72322474a1ba15871d90697
Bipingrg-code <bipin67678grg@gmail.com>
2026-06-04 15:04:48 +0545

    showcase section with animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './index.css'
 import Hero from './sections/Hero'
+import ShowcaseSection from './sections/ShowcaseSection'
 
 export default function App() {
   return (
     <main>
       <Hero />
+      <ShowcaseSection />
     </main>
   )
 }
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -9,6 +9,13 @@ const Hero = () => {
     useGSAP(()=>{
         gsap.fromTo('.hero-text h1',{y:50,opacity:0},{y:0,opacity:1,duration:2,stagger:0.3,ease:'power2.inOut'})
     })
+    // useGSAP(() => {
+    //     gsap.fromTo(
+    //         '.hero-text h1',
+    //         { y: 50, opacity: 0 },
+    //         { y: 0, opacity: 1, duration: 2, stagger: 0.3, ease: 'power2.inOut' }
+    //     )
+    // }, [])
     return (
         <section id="hero" className="relative overflow-hidden">
             <div className="absolute top-0 left-0 z-10">
